01cead79e2d3feaff4544914dd12df6da84de819
end-2 <sejongchoi.400@gmail.com>
2024-04-20 00:05:36 +0900

    Create simple chat app

--- doc/rag.md
+++
@@ -1,4 +0,0 @@
-https://www.rungalileo.io/blog/mastering-rag-how-to-select-an-embedding-model
-https://www.mongodb.com/developer/products/atlas/choose-embedding-model-rag/
-https://huggingface.co/spaces/mteb/leaderboard
-https://github.com/BradyFU/Awesome-Multimodal-Large-Language-Models/blob/Evaluation/README.md
--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import GPT4AllEmbeddings
-from memory import Memory
+import logging
+import dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage
+from langchain_core.messages import AIMessage
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-embedding_model = GPT4AllEmbeddings()
-memory = Memory(text_splitter, embedding_model)
 
-loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
-question = "What are the approaches to Task Decomposition?"
+logging.basicConfig(level=logging.INFO,
+                    filename='app.log',
+                    filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-memory.load(loader)
+logger = logging.getLogger(__name__)
 
-for doc in memory.retrieve(question):
-    print(doc)
+logger.info("Starting chat app")
+
+dotenv.load_dotenv()
+
+chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer all questions to the best of your ability.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+chain = prompt | chat
+
+messages = []
+
+while True:
+    logger.info("Start chat loop")
+
+    user_input = input("Please enter message: ")
+    logger.info("User input: %s", user_input)
+
+    if user_input == "exit":
+        break
+
+    messages.append(HumanMessage(content=user_input))
+    logger.info("Messages: %s", messages)
+
+    resp = chain.invoke({"messages": messages})
+    logger.info("Response: %s", resp)
+
+    print(resp.content)
+    messages.append(AIMessage(content=resp.content))
